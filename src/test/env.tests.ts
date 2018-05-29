7e5ac597fa198af5fe67c7c8525ff0e8db2661f7
Fons Eppink <fonseppink@hotmail.com>
2018-05-29 14:04:25 +0200

    fix: Add support to override travis branch

--- src/test/env.tests.ts
+++ src/test/env.tests.ts
@@ -3,11 +3,23 @@ import { load } from '../env';
 import * as path from 'path';
 
 const expect = Chai.expect;
+const oldEnv = Object.assign({}, process.env);
 describe('load', () => {
+  afterEach(() => {
+    process.env = Object.assign({}, oldEnv);
+  });
+
   it('loads the default js.', () => {
-    const oldEnv = Object.assign({}, process.env);
     load(path.resolve(__dirname, 'mock/'));
     expect(process.env.testSetting).to.be.equal('Set');
-    process.env = oldEnv;
+  });
+
+  it('loads the default js and a branch js and branch config file overrules default setting.', () => {
+    load(path.resolve(__dirname, 'mock/'), 'testbranch');
+    expect(process.env.testSetting).to.be.equal('test branch yo');
+  });
+
+  it('gives no error with invalid path.', () => {
+    load('');
   });
 });
