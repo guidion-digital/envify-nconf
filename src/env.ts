2ccb6b1244373726e7742853070311147a8fb668
maardje <maartje.snoeckhenkemans@gmail.com>
2018-06-12 13:40:27 +0200

    style(): some stylistic changes

--- src/env.ts
+++ src/env.ts
@@ -1,14 +1,11 @@
-import * as path from 'path';
-import * as nconf from 'nconf';
+import {join} from 'path';
+import {argv} from 'nconf';
 
-function load(configFolder: string, branch = process.env.TRAVIS_PULL_REQUEST_BRANCH || process.env.TRAVIS_BRANCH) {
-
-  process.env = nconf.argv()
+export const load = (configFolder: string, branch = process.env.TRAVIS_PULL_REQUEST_BRANCH || process.env.TRAVIS_BRANCH) => {
+  process.env = argv()
     .env()
-    .file('local', {file: path.join(configFolder, `/local.json`)})
-    .file('branch', {file: path.join(configFolder, `/${branch}.json`)})
-    .file('defaults', {file: path.join(configFolder, '/default.json')})
+    .file('local', {file: join(configFolder, `/local.json`)})
+    .file('branch', {file: join(configFolder, `/${branch}.json`)})
+    .file('defaults', {file: join(configFolder, '/default.json')})
     .get();
-}
-
-export {load};
+};
