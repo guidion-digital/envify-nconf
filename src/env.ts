8918da2ad2bfcbd55084cb02c21af6c20baf5633
Fons Eppink <fonseppink@hotmail.com>
2018-06-20 11:31:33 +0200

    chore: Fix the merge conflict

--- src/env.ts
+++ src/env.ts
@@ -1,10 +1,10 @@
 import {join} from 'path';
 import {argv} from 'nconf';
 
-function load(
+export const load = (
   configFolder: string = `${process.cwd()}/config`,
   branch: string = process.env.TRAVIS_PULL_REQUEST_BRANCH || process.env.TRAVIS_BRANCH
-) {
+) => {
   process.env = argv()
     .env()
     .file('local', {file: join(configFolder, `/local.json`)})
